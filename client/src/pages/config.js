af7832a17c88c74ec130776accbb1e2973766b01
rohits21381 <rohit.shinde9509@gmail.com>
2025-04-11 09:39:47 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,7 +1,7 @@
 // // const API_BASE_URL = "http://localhost:8800";
 // const API_BASE_URL = "http://veera.narni.co.in";
 // export default API_BASE_URL;
-// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
+// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL 
 // export default API_BASE_URL;
 const API_BASE_URL = "http://api.rohitsh.in";
 export default API_BASE_URL;
